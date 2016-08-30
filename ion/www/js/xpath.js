106fe007ef81924ad4121042b265bcb28713b949
pax <coolwinding@gmail.com>
2016-08-30 10:20:52 +0800

    Add xloader (lib loader)
    Now we can define lib path in standalone js file instead of html file,
    then both product and unit-testing can load libs from the same config.

---
+++ ion/www/js/xpath.js
@@ -0,0 +1,79 @@
+(function() {
+    'use strict';
+
+    // Define all css, js file's path.
+    // Note:
+    // - All paths are related to root path `www`.
+    // - Some libs do not have runable min-version or normal-version, then here just use runable version.
+    var paths = {
+        css: {
+            lib: [],
+            libMin: [],
+            core: [],
+            mod: []
+        },
+        js: {
+            lib: [],
+            libMin: [],
+            core: [],
+            mod: [],
+            test: []
+        }
+    };
+
+    var xpath = {};
+
+    // Get special path with `prefix`
+    xpath.getPath = function(prefix) {
+        if (typeof(prefix) !== 'string') {
+            return paths;
+        }
+        // deep copy
+        try {
+            var paths2 = JSON.parse(JSON.stringify(paths));
+        } catch (error) {
+            return paths;
+        }
+        return (function addPrefix(path) {
+            if (typeof(path) === 'string') {
+                return prefix + path;
+            } else if (typeof(path) === 'object') {
+                for (var i in path) {
+                    path[i] = addPrefix(path[i]);
+                }
+                return path;
+            } else {
+                return path;
+            }
+        })(paths2);
+    }
+
+    // path for unit-testing
+    xpath.path4Test = xpath.getPath('www/');
+    // path for product
+    xpath.path4Product = xpath.getPath();
+
+    // Establish the root object, `window` (`self`) in the browser, `global`
+    // on the server, or `this` in some virtual machines. We use `self`
+    // instead of `window` for `WebWorker` support.
+    var root = typeof self === 'object' && self.self === self && self ||
+        typeof global === 'object' && global.global === global && global ||
+        this;
+
+    // Node.js
+    if (typeof module === 'object' && module.exports) {
+        module.exports = xpath;
+    }
+    // AMD / RequireJS
+    else if (typeof define === 'function' && define.amd) {
+        define([], function() {
+            return xpath;
+        });
+    }
+    // included directly via <script> tag
+    else {
+        root.xpath = xpath;
+    }
+
+})();
+
