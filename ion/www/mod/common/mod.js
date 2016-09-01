88660cef766106e612b243f4606a08b8899b531c
pax <coolwinding@gmail.com>
2016-09-01 15:23:53 +0800

    Add mod:common

---
+++ ion/www/mod/common/mod.js
@@ -0,0 +1,18 @@
+(function() {
+    'use strict';
+
+    /**
+     * @description
+     * Common is module to define common css, i18n...
+     *
+     * @memberof mod
+     * @ngdoc overview
+     * @name mod.common
+     * @requires service
+     * @requires util
+     * @requires protocol
+     */
+    angular.module('mod.common', ['service', 'util', 'protocol'])
+
+})();
+
